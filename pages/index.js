38288cf9581ce7feac442162cad267dec4947cc6
Luan Silveira Pontolio <lpontolio.mooven@bionexo.com>
2020-08-20 21:55:02 -0300

    chore: Add component Avatar

--- pages/index.js
+++ pages/index.js
@@ -1,61 +1,45 @@
 import Head from 'next/head'
+import Avatar from '../components/Avatar.js'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Profile</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
+        <Avatar />
+        <h2>Hi !! I'm Luan</h2>
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="https://www.linkedin.com/in/luanpontolio/" target="_blank" className="card">
+            <h3>LinkedIn</h3>
+            <p>My professional profile</p>
           </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          <a href="https://github.com/luanpontolio" target="_blank" className="card">
+            <h3>Github</h3>
+            <p>My personal projects</p>
           </a>
-
-          <a
-            href="https://github.com/zeit/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          <a href="mailto:luan.pontolio@gmail.com" target="_blank" className="card">
+            <h3>Gmail</h3>
+            <p>Contact me here</p>
           </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+          <a href="https://www.instagram.com/luan_spontolio/" target="_blank" className="card">
+            <h3>Instagram</h3>
+            <p>Follow me</p>
           </a>
         </div>
       </main>
 
       <footer>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="#"
           target="_blank"
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
+          Luan
         </a>
       </footer>
 
